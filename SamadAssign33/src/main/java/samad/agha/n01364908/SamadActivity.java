2603e67a845ed9ad43bcb0ebf90dd5ac79145b04
SamadAgha4908 <n01364908@humbermail.ca>
2021-03-28 19:15:49 -0400

    - Added Tween animation - Fixed Frame animation overlapping - Removed hardcoded text

--- SamadAssign33/src/main/java/samad/agha/n01364908/SamadActivity.java
+++ SamadAssign33/src/main/java/samad/agha/n01364908/SamadActivity.java
@@ -1,44 +1,26 @@
 //Samad Agha n01364908 C
 package samad.agha.n01364908;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Camera;
+import android.content.DialogInterface;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AlertDialog;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import samad.agha.n01364908.ui.main.SectionsPagerAdapter;
-import samad.agha.n01364908.CanvasView;
-
-import static java.security.AccessController.getContext;
 
 
 public class SamadActivity extends AppCompatActivity {
@@ -61,26 +43,7 @@ public class SamadActivity extends AppCompatActivity {
 
     }
 
-    // Handle Start Button frame frame
-    public void starting(View view)
-    {
-        final Button onButton = (Button) findViewById(R.id.ButtonStart);
-        onButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startAnimation();
-            }
-        });
-    }
 
-    // Handle Stop Button
-    public void stopping(View view){
-    final Button offButton = (Button) findViewById(R.id.ButtonStop);
-        offButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            stopAnimation();
-        }
-    });
-        }
 
     public void redColor(View view){
         CanvasView.mPaint.setColor(Color.RED);
@@ -104,10 +67,10 @@ public class SamadActivity extends AppCompatActivity {
         Canvasview.resetPath();
     }
 
-    private void startAnimation()
+    public void startAnimation(View view)
     {
 
-        ImageView img = (ImageView)findViewById(R.id.imageView_Boy);
+        ImageView img = (ImageView)findViewById(R.id.imageViewBanana);
 
         BitmapDrawable frame1 = (BitmapDrawable)getResources().getDrawable(R.drawable.banana0);
         BitmapDrawable frame2 = (BitmapDrawable)getResources().getDrawable(R.drawable.banana1);
@@ -118,8 +81,6 @@ public class SamadActivity extends AppCompatActivity {
         BitmapDrawable frame7 = (BitmapDrawable)getResources().getDrawable(R.drawable.banana6);
         BitmapDrawable frame8 = (BitmapDrawable)getResources().getDrawable(R.drawable.banana7);
 
-
-        // Get the background, which has been compiled to an AnimationDrawable object.
         int reasonableDuration = 250;
         mframeAnimation = new AnimationDrawable();
         mframeAnimation.setOneShot(false);	// loop continuously
@@ -137,7 +98,7 @@ public class SamadActivity extends AppCompatActivity {
         mframeAnimation.setVisible(true,true);
         mframeAnimation.start();
     }
-    private void stopAnimation()
+    public void stopAnimation(View view)
     {
         mframeAnimation.stop();
         mframeAnimation.setVisible(false,false);
@@ -148,7 +109,6 @@ public class SamadActivity extends AppCompatActivity {
         this.ImageViewearth = findViewById(R.id.earthImgView);
         this.ImageViewmoon = findViewById(R.id.moonImgView);
 
-        // add event handlers
         findViewById(R.id.startAnimBtn).setOnClickListener(v -> this._startAnimation());
     }
 
@@ -159,34 +119,43 @@ public class SamadActivity extends AppCompatActivity {
         findViewById(R.id.stopAnimBtn).setOnClickListener(v -> this._stopAnimation());
     }
 
-    //helper methods
     private void _startAnimation() {
-        Toast.makeText(this, getResources().getString(R.string.startanim_txt), Toast.LENGTH_SHORT).show();
-        // we will animate the imageview
         this.ImageViewearth.setImageResource(R.drawable.earth);
         this.ImageViewmoon.setImageResource(R.drawable.moonog);
 
-        // Load the appropriate animation
         Animation earthAnim = AnimationUtils.loadAnimation(this, R.anim.spin);
         Animation moonAnim = AnimationUtils.loadAnimation(this, R.anim.spin2);
 
         earthAnim.setRepeatCount(Animation.INFINITE);
         moonAnim.setRepeatMode(Animation.RESTART);
         moonAnim.setRepeatCount(Animation.INFINITE);
-        // Register a listener, so we can disable and re-enable buttons
-//        an.setAnimationListener(new MyAnimationListener());
-        // Start the animation
+
         this.ImageViewmoon.startAnimation(moonAnim);
         this.ImageViewearth.startAnimation(earthAnim);
     }
 
     private void _stopAnimation() {
-        Toast.makeText(this, getResources().getString(R.string.stopanim_txt), Toast.LENGTH_SHORT).show();
         this.ImageViewearth.clearAnimation();
         this.ImageViewmoon.clearAnimation();
-        // remove image resources
-//        this._earthImageView.setImageResource(0);
-//        this._moonImageView.setImageResource(0);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.ic_baseline_warning_24)
+                .setTitle(getString(R.string.ExitConfirmTitle))
+                .setMessage(getString(R.string.ExitConfirm))
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
 
